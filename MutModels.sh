01283d18db709969fc2f27f2a8741f33cfd63b4b
subject-am <31853642+subject-am@users.noreply.github.com>
2019-10-26 22:41:46 +0100

    Create MutModels.sh

---
+++ MutModels.sh
@@ -0,0 +1,9 @@
+while read fasta ; do echo P1P > ${fasta%%.fasta}_pattern ; done < consensus.log
+for pattern in $(ls *_pattern) ; do sh pattern-HIVp.sh 1NH0_ref.pdb $pattern ; done
+
+mkdir -p ../1NH0_pdb-files
+for folder in $(ls -d 1NH0_ref_*_pattern)
+do
+	mv ${folder}/$(ls $folder --sort=time | head -n 1) ../1NH0_pdb-files/${folder}.pdb # most recent
+	rm -r ${folder}
+done
