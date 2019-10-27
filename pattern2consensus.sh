5f782456a407ce7c4a447476d614fe0c64105daf
subject-am <31853642+subject-am@users.noreply.github.com>
2019-10-27 10:56:41 +0000

    Create pattern2consensus.sh

---
+++ pattern2consensus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Compare with susceptible
+for file in $(ls *.fasta)
+do
+	for position in {0..98}
+	do
+		seqcons=PQITLWQRPLVTIKIGGQLKEALLDTGADDTVLEEMNLPGRWKPKMIGGIGGFIKVRQYDQILIEICGHKAIGTVLVGPTPVNIIGRNLLTQIGCTLNF
+		sequence=$(sed '1d' $file)
+		ressus=${seqcons:$position:1}
+		resseq=${sequence:$position:1}
+		respos=$(expr $position + 1)
+		if [[ $ressus != $resseq ]]
+		then
+			echo $resseq$respos$ressus >> ${file%%.fasta}_pattern
+		fi
+	done
+done
